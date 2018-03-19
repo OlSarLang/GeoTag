a7213bdc54224582892a093eefd8ce8b6305939d
SW4GL0RD <o.sarholm@hotmail.com>
2018-03-19 21:07:41 +0100

    Added two classes, Field and Marker with their strings.

---
+++ app/src/main/java/com/example/left4candy/geotag/Marker.java
@@ -0,0 +1,25 @@
+package com.example.left4candy.geotag;
+
+
+public class Marker {
+    public int markerId;
+    private String markerColor;
+    private String markerType;
+
+    public String getMarkerColor() {
+        return markerColor;
+    }
+
+    public void setMarkerColor(String markerColor) {
+        this.markerColor = markerColor;
+    }
+
+    public String getMarkerType() {
+        return markerType;
+    }
+
+    public void setMarkerType(String markerType) {
+        this.markerType = markerType;
+    }
+
+}

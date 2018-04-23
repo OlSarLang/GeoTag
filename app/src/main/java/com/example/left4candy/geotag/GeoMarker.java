4d74f0db5556e24f410e678e2ff25a4b65e1e443
SW4GL0RD <o.sarholm@hotmail.com>
2018-04-23 22:46:40 +0200

    Uploading and creating GeoMarker success !

--- app/src/main/java/com/example/left4candy/geotag/GeoMarker.java
+++ app/src/main/java/com/example/left4candy/geotag/GeoMarker.java
@@ -1,54 +1,83 @@
 package com.example.left4candy.geotag;
 
 
-public class GeoMarker {
-    private int geoMarkerId;
+public class GeoMarker{
+    private static int geoMarkerId;
     private String geoMarkerName;
     private String geoMarkerColor;
-    private float geoMarkerLat;
-    private float geoMarkerLong;
+    private double geoMarkerLat;
+    private double geoMarkerLong;
+    private String firstName;
+    private String secondName;
+    private String thirdName;
     private String firstField;
     private String secondField;
     private String thirdField;
 
     public GeoMarker(){
+        this("name", "color", 0, 0);
+        firstName = "Name One";
+        secondName = "Name Two";
+        thirdName = "Name Three";
+        firstField = "Field One";
+        secondField = "Field Two";
+        thirdField = "Field Three";
+        geoMarkerId++;
     }
 
-    public GeoMarker(String geoMarkerName, String geoMarkerColor, float geoMarkerLat, float geoMarkerLong){
+    public GeoMarker(String geoMarkerName, String geoMarkerColor, double geoMarkerLat, double geoMarkerLong){
         this.geoMarkerName = geoMarkerName;
         this.geoMarkerColor = geoMarkerColor;
         this.geoMarkerLat = geoMarkerLat;
         this.geoMarkerLong = geoMarkerLong;
-        this.firstField = "Field One";
-        this.secondField = "Field Two";
-        this.thirdField = "Field Three";
-
+        firstName = "Name One";
+        secondName = "Name Two";
+        thirdName = "Name Three";
+        firstField = "Field One";
+        secondField = "Field Two";
+        thirdField = "Field Three";
+        geoMarkerId++;
     }
 
     public int getGeoMarkerId() {
         return geoMarkerId;
     }
 
-    public String getGepMarkerColor() {
+    public String getGeoMarkerColor() {
         return geoMarkerColor;
     }
     public void setGeoMarkerColor(String geoMarkerColor) {
         this.geoMarkerColor = geoMarkerColor;
     }
 
-    public String geoMarkerName(){return geoMarkerName;}
+    public String getGeoMarkerName(){return geoMarkerName;}
     public void setGeoMarkerName(String geoMarkerName){
         this.geoMarkerName = geoMarkerName;
     }
 
     public double getGeoMarkerLong() {return geoMarkerLong;}
-    public void setGeoMarkerLong(float geoMarkerLong) {
+    public void setGeoMarkerLong(double geoMarkerLong) {
         this.geoMarkerLong = geoMarkerLong;
     }
 
     public double getGeoMarkerLat() {return geoMarkerLat;}
-    public void setGeoMarkerLat(float geoMarkerLat) {
+    public void setGeoMarkerLat(double geoMarkerLat) {
         this.geoMarkerLat = geoMarkerLat;
+    }
+
+    public String getFirstName() {return firstName; }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {return secondName; }
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getThirdName() {return thirdName; }
+    public void setThirdName(String thirdName) {
+        this.thirdName = thirdName;
     }
 
     public String getFirstField() { return firstField; }
